e32a5522babbf34cf5d1e2ea4f5adfdf77ff70de
AbhikPatel <abhikpatel15@gmail.com>
2023-03-13 15:53:01 +0530

    fix: fixed bug of connection

--- src/app/shared/common.service.ts
+++ src/app/shared/common.service.ts
@@ -1,25 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
 
-  public api: string = 'http://172.16.3.107:21321/api/v1/users'
   public loader:BehaviorSubject<boolean>
-
+  public api:string;
   constructor(
     private _http: HttpClient
   ) {
     this.loader = new BehaviorSubject(false);
+    this.api = environment.baseURL
   }
 
   
 
   public postLogin(credentials:any):Observable<any>{
-    return this._http.post<any>(`${this.api}/log-in`, credentials).pipe(
+    return this._http.post<any>(`${this.api}/users/log-in`, credentials).pipe(
       map((res:any) => res)
     )
   }
